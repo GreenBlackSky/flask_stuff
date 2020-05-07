199f1418bce26e7ce8b1586bd63a372f99f6db58
GreenBlackSky <greenblacksky@gmail.com>
2020-05-07 20:47:40 +0300

    finishing redis

--- redis/redis_test_app/__init__.py
+++ redis/redis_test_app/__init__.py
@@ -2,14 +2,11 @@ from flask import Flask
 from flask_session import Session
 
 
-sess = Session()
-
-
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
 
     app.config.from_object('config.Config')
-    sess.init_app(app)
+    Session(app)
 
     with app.app_context():
         from . import routes
--- redis/redis_test_app/routes.py
+++ redis/redis_test_app/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, session
 from .forms import AddItemForm, RemoveItemForm
-from . import sess
+
 
 bp = Blueprint('main_bp', __name__, template_folder='templates')
 
@@ -13,11 +13,11 @@ def dashboard():
     )
 
 
-@bp.route('/add_item')
+@bp.route('/add_item', methods=('GET', 'POST'))
 def add_item():
     form = AddItemForm()
     if form.validate_on_submit():
-        sess[form.key.data] = form.value.data
+        session[form.key.data] = form.value.data
         return redirect(url_for('main_bp.dashboard'))
     return render_template(
         'add_item.html',
@@ -26,14 +26,15 @@ def add_item():
     )
 
 
-@bp.route('/remove_item')
+@bp.route('/remove_item', methods=('GET', 'POST'))
 def remove_item():
     form = RemoveItemForm()
     if form.validate_on_submit():
-        sess.pop(form.key.data)
+        session.pop(form.key.data)
         return redirect(url_for('main_bp.dashboard'))
     return render_template(
         'remove_item.html',
-        title='Remove item from redis session.'
+        title='Remove item from redis session.',
+        form=form
     )
 
