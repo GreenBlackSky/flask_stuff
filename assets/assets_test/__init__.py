7893f7bade9b2ba1e14974619f2acb64cb5d6387
GreenBlackSky <greenblacksky@gmail.com>
2020-05-09 22:49:43 +0300

    js added to assets

--- assets/assets_test/__init__.py
+++ assets/assets_test/__init__.py
@@ -1,15 +1,10 @@
 from flask import Flask
-from flask_assets import Environment, Bundle
-
-
-assets = Environment()
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    assets.init_app(app)
 
     with app.app_context():
         from . import routes
